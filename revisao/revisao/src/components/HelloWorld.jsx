23d9b538be31c2605d807460dc759ac187dd5277
Anthony Moreira <anthonysteffano@alu.ufc.br>
2020-07-27 19:03:02 -0300

    master

--- revisao/revisao/src/components/HelloWorld.jsx
+++ revisao/revisao/src/components/HelloWorld.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
+//Versão2
+export default()=>
+<div>
+    <h2>Desenvolvimento de Software para WEB - SI versao 2</h2>
+</div>
+
+
 //Versão 1
-export default()=>{
-   return( <div>
-        <h2>Desenvolvimento de Software para WEB - SI V1</h2>
-    </div>
-   )
-}
+// export default()=>{
+//    return( <div>
+//         <h2>Desenvolvimento de Software para WEB - SI V1</h2>
+//     </div>
+//    )
+// }
 
 
 // Versão 0
