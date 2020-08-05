82629b4ba49637d25cdba7f49fc32f4421410f59
Anthony Moreira <anthonysteffano@alu.ufc.br>
2020-08-04 22:52:05 -0300

    master

--- atividade_2/atividade_2/src/App.js
+++ atividade_2/atividade_2/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 import Q01 from './components/questao1/Arena'
 
-import Q03 from './components/questao3/inicio'
+import Q03 from './components/questao3/Inicio'
 
-import Q04 from './components/questao4/inicio'
+import Q04 from './components/questao4/Inicio'
 
 function App() {
   return (
